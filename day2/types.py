b854a60b6336211d11adafa2005539e30a0b781a
German Uritskiy <guritsk1@jhu.edu>
2015-08-25 10:38:23 -0400

    second file

---
+++ day2/types.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+#integer
+i=123
+
+#float
+f=1.23444
+
+#strng
+s="aasdf42"
+
+#bool
+t1=True
+f1=False
+
+#dictionary
+dic={"a":1,"b":2,"c":3}
+
+#lists - these can be modified
+lis=[1,2,3,4,5,6]
+lis.append(7)
+
+#tuple - can have different types
+tup=(1,"sdf",4.34)
+
+#loops
+
+for i in (1,2,3,4,"sdf","a",True):
+    print i
